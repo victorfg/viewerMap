20ce909ad2a9aeaac93df6f7eb751f0fc09793d8
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-22 19:01:52 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,39 @@
 import '../styles/globals.css'
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <ErrorBoundary>
+      <Component {...pageProps} />
+    </ErrorBoundary>
+  )
+}
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    // You can also log the error to an error reporting service
+    logErrorToMyService(error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children; 
+  }
 }
 
 export default MyApp
