c78fb833e04602f1577811c9f3ec33ff8bc1658f
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-23 11:06:22 +0100

    .

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ import {NavigationItems} from '../components/menu/NavigationItems'
 import MenuToggle from '../components/menu/MenuToogle'
 import {sidebarTransition} from '../components/constants'
 
-export default function Home() {
+export default function HomeMap() {
   const [openMenuOptions, setOpenMenuOptions] = useState(false);
   const [selectedBaseLayer, setSelectedBaseLayer] = useState({ ORTOFOTOMAPA_MAP: true, TOPOGRAFIC_MAP: false });
   const [selectLayers, setSelectLayers] = useState({ COMARQUES_LAYER:false, MUNICIPIS_LAYER:false });
@@ -55,82 +55,79 @@ export default function Home() {
       }
       setOpacityLayer(newObj);
   }
-  try {
-    return (
-        <div className="grid-container">
-            <MenuComponent 
-                selectedBaseLayer={selectedBaseLayer} 
-                openMenuOptions={openMenuOptions}
-                handlerRadioButtonsBaseLayer={handlerRadioButtonsBaseLayer}
-                handlerCheckButtonsLayers={handlerCheckButtonsLayers}
-                handlerOpacityLayer={handlerOpacityLayer}
-            />    
-            <div id="map" className="map">
-                <div id="popup" className="ol-popup">
-                    <a href="#" id="popup-closer" className="ol-popup-closer"></a>
-                    <div id="popup-content"></div>
-                </div>
-                <motion.nav
-                    className="menu"
-                    initial={false}
-                    animate={isOpen ? "open" : "closed"}
-                    custom={height}
-                    ref={containerRef} 
-                >
-                    <motion.div className="background" variants={sidebarTransition} />
-                    <NavigationItems />
-                    <MenuToggle toggle={() => toggleOpen()} />
-                    {/*<div id="menuLeft" className="bar-menu-left" onClick={() => setOpenMenuOptions(prevState => !prevState)}>
-                        <svg className="h-8 w-8"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
-                        </svg>
-                    </div>*/}            
-                </motion.nav>
-                <Map 
-                    selectedBaseLayer={selectedBaseLayer} 
-                    selectLayers={selectLayers}
-                    opacityLayer={opacityLayer}
-                >
-                    <Layers>
-                        {selectedBaseLayer.TOPOGRAFIC_MAP && (
-                            <BaseLayers
-                                source={topo()}
-                                title={baseLayers.TOPOGRAFIC_MAP}
-                            />
-                        )}
-                        {selectedBaseLayer.ORTOFOTOMAPA_MAP && (
-                            <BaseLayers
-                                source={orto()}
-                                title={baseLayers.ORTOFOTOMAPA_MAP}
-                            />
-                        )}
-                        
-                        {selectLayers.COMARQUES_LAYER && (
-                            <GroupLayers
-                                source={comarques()}
-                                title={layers.COMARQUES_LAYER}
-                                selectedBaseLayer={selectedBaseLayer}
-                            />
-                        )}
-    
-                        {selectLayers.MUNICIPIS_LAYER && (
-                            <GroupLayers
-                                source={municipis()}
-                                title={layers.MUNICIPIS_LAYER}
-                                selectedBaseLayer={selectedBaseLayer}
-                            />
-                        )}
-                        {/*<VectorLayer usersData={usersData}/>*/}
-                    
-                    </Layers>
-                    {/*<Controls>
-                        <FullScreenControl />
-                    </Controls>*/}
-                </Map>
-            </div>
-        </div>
-      ); 
-  } catch (error) {
-    console.log(error)
-  }
+
+  return (
+      <div className="grid-container">
+          <MenuComponent 
+              selectedBaseLayer={selectedBaseLayer} 
+              openMenuOptions={openMenuOptions}
+              handlerRadioButtonsBaseLayer={handlerRadioButtonsBaseLayer}
+              handlerCheckButtonsLayers={handlerCheckButtonsLayers}
+              handlerOpacityLayer={handlerOpacityLayer}
+          />    
+          <div id="map" className="map">
+              {/*<div id="popup" className="ol-popup">
+                  <a href="#" id="popup-closer" className="ol-popup-closer"></a>
+                  <div id="popup-content"></div>
+              </div>*/}
+              <motion.nav
+                  className={`menu ${isOpen ? 'z-10' : ''}`}
+                  initial={false}
+                  animate={isOpen ? "open" : "closed"}
+                  custom={height}
+                  ref={containerRef} 
+              >
+                  <motion.div className="background" variants={sidebarTransition} />
+                  <NavigationItems />
+                  <MenuToggle toggle={() => toggleOpen()} />
+                  {/*<div id="menuLeft" className="bar-menu-left" onClick={() => setOpenMenuOptions(prevState => !prevState)}>
+                      <svg className="h-8 w-8"  fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                          <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16M4 18h16"/>
+                      </svg>
+                  </div>*/}            
+              </motion.nav>
+              <Map 
+                  selectedBaseLayer={selectedBaseLayer} 
+                  selectLayers={selectLayers}
+                  opacityLayer={opacityLayer}
+              >
+                  <Layers>
+                      {selectedBaseLayer.TOPOGRAFIC_MAP && (
+                          <BaseLayers
+                              source={topo()}
+                              title={baseLayers.TOPOGRAFIC_MAP}
+                          />
+                      )}
+                      {selectedBaseLayer.ORTOFOTOMAPA_MAP && (
+                          <BaseLayers
+                              source={orto()}
+                              title={baseLayers.ORTOFOTOMAPA_MAP}
+                          />
+                      )}
+                      
+                      {selectLayers.COMARQUES_LAYER && (
+                          <GroupLayers
+                              source={comarques()}
+                              title={layers.COMARQUES_LAYER}
+                              selectedBaseLayer={selectedBaseLayer}
+                          />
+                      )}
+
+                      {selectLayers.MUNICIPIS_LAYER && (
+                          <GroupLayers
+                              source={municipis()}
+                              title={layers.MUNICIPIS_LAYER}
+                              selectedBaseLayer={selectedBaseLayer}
+                          />
+                      )}
+                      {/*<VectorLayer usersData={usersData}/>*/}
+                  
+                  </Layers>
+                  {/*<Controls>
+                      <FullScreenControl />
+                  </Controls>*/}
+              </Map>
+          </div>
+      </div>
+  )
 }
