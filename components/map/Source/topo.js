763521e60b5ceaf54cd2fbc557dca9bc2343ac94
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-23 16:07:46 +0100

    .

--- components/map/Source/topo.js
+++ components/map/Source/topo.js
@@ -2,9 +2,9 @@ import TileWMS from 'ol/source/TileWMS';
 
 function topo() {
 	return new TileWMS({
-        url: 'http://mapcache.icc.cat/map/bases/service?',
+        url: 'https://geoserveis.icgc.cat/icc_mapesbase/wms/service?',
         params: {
-            'LAYERS': 'topo'
+            'LAYERS': 'mtc5m'
         }
     })
 }
