508d02df94fa77cbadf720673785f884a6e4a039
Victor Fernandez Gayan <victorfgay@ext.stradivarius.com>
2022-12-28 14:03:20 +0100

    .

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '../styles/globals.css'
 import React from 'react';
+import Script from 'next/script'
 import  MapContextProvider  from '../store/contexts/MapContextProvider';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      {/*<Script src="https://cdn.jsdelivr.net/npm/elm-pep@1.0.6/dist/elm-pep.js"></Script>*/}
       <MapContextProvider>
         <Component {...pageProps} />
       </MapContextProvider>
